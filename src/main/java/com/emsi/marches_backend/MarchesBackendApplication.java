adbffa0081cedbb6f80238f6f2e091403fc24cc0
chaimaa qchiine <162720400+Qchiine@users.noreply.github.com>
2026-03-06 04:37:03 +0000

    Initial commit - backend Spring Boot + Docker + MongoDB

---
+++ src/main/java/com/emsi/marches_backend/MarchesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.emsi.marches_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarchesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarchesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emsi/marches_backend/MarchesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emsi.marches_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarchesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
